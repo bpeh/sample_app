00f2e72c290c6b4aed8953acb19e6caecb49b315
Bernard Peh <bernard.peh@sitepoint.com>
2011-02-26 12:45:47 +1100

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '1fc31bae5fc9b94d81531a9bd9eae380c8571039a6aef9c682b2b5e41c42836105d2bf5c1d7e1994307cbc3f65b7010c6003867a32e6e34a544e46171f47c0a4'
